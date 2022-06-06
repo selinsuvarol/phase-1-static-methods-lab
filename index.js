f33576b38cd1da4fd2eebbb1b8d1db5057cfaa99
Selin Su Varol <selinsuvarol@gmail.com>
2022-06-06 12:29:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize( str) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let dontCapitalize = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let wordsArray = sentence.split( " " );
+
+    for ( let i = 0; i < wordsArray.length; i++ ) {
+      if ( i == 0 ) {
+        result.push( this.capitalize( wordsArray[i] ) )
+      }
+      else {
+        if ( dontCapitalize.includes( wordsArray[i] ) ) {
+          result.push( wordsArray[i] )}
+        else {
+          result.push( this.capitalize( wordsArray[i] ) )}
+      }
+    }
+    return result.join( " " );
+  }
 }
